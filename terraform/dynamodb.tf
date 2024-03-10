a64cea7b35505db05fdbdf2d6277acadefd2582e
NonokaM <c1201071@gmail.com>
2024-03-11 04:40:09 +0900

    add: dyanmodb.tf

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,12 @@
+resource "aws_dynamodb_table" "hackit_table" {
+  name           = "hackit_table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
