5deca50e8aee6923d03576a293ad21a8c96a80dc
NonokaM <c1201071@gmail.com>
2024-03-12 00:33:12 +0900

    update: db schema

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "hackit_table" {
-  name           = "hackit_table"
+  name           = "hackit_questions_table"
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
@@ -7,6 +7,6 @@ resource "aws_dynamodb_table" "hackit_table" {
 
   attribute {
     name = "id"
-    type = "S"
+    type = "N"
   }
 }
