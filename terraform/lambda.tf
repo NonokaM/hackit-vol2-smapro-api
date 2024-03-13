ede1302545d9855f2cff8e1379e85012f13149c1
NonokaM <c1201071@gmail.com>
2024-03-14 02:09:04 +0900

    add:

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,12 +3,11 @@ resource "aws_lambda_function" "fetch_questions_function" {
   role          = aws_iam_role.hackit_lambda_role.arn
 
   // Lambda関数のDockerイメージをECRから参照
-  image_uri = ""
+  image_uri = "your_ecr_repository_uri"
 
   package_type = "Image"
 }
 
-// Lambda関数のためのIAMロール
 resource "aws_iam_role" "hackit_lambda_role" {
   name = "hackit_lambda_role"
 
