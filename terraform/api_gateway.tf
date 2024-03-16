057c7cdd2fa1d4fb84f500f91650055cab7fab18
NonokaM <c1201071@gmail.com>
2024-03-17 00:02:22 +0900

    update: cors setting

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -28,6 +28,37 @@ resource "aws_api_gateway_integration" "questions_lambda_integration" {
   uri                     = aws_lambda_function.fetch_questions_function.invoke_arn
 }
 
+resource "aws_api_gateway_method_response" "questions_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.hackit_smapro_api.id
+  resource_id = aws_api_gateway_resource.questions_resource.id
+  http_method = aws_api_gateway_method.questions_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "questions_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.hackit_smapro_api.id
+  resource_id = aws_api_gateway_resource.questions_resource.id
+  http_method = aws_api_gateway_method.questions_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.questions_method,
+    aws_api_gateway_integration.questions_lambda_integration
+  ]
+}
+
 # API Gatewayのデプロイメント
 resource "aws_api_gateway_deployment" "hackit_api_gateway_deployment" {
   depends_on = [aws_api_gateway_integration.questions_lambda_integration]
